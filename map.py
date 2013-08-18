c61899d4814224b7e7511fbcb47dd4d46379f508
CharlesRandles <charles.randles@gmail.com>
2013-08-18 13:10:31 +1000

    Server side now works fully. (Client side works somewhat flakily)

--- map.py
+++ map.py
@@ -7,9 +7,89 @@ cgitb.enable()
 
 LOCATION_FILENAME="chaz_location.txt"
     
+#OK, now write a map
+
+def http_headers():
+    return """Content-Type: text/html
+
+    """
+
+def google_map(lat, long):
+    """ Write html/js code for creating a google map """
+    return """
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+    <style type="text/css">
+      html { height: 100% }
+      body { height: 100%; margin: 0; padding: 0 }
+      #map-canvas { height: 100% }
+    </style>
+    <script type="text/javascript"
+      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBjgdfoiHLvChKKGfqOQHryhFyWapPPzy0&sensor=false">
+    </script>
+   <script type="text/javascript">
+      function initialize() {
+        var latLng = new google.maps.LatLng(""" + lat + "," + long + """);
+        var mapOptions = {
+          center: latLng,
+          zoom: 12,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("map-canvas"),
+            mapOptions);
+        var marker = new google.maps.Marker({
+            position: latLng,
+            map: map,
+            title: 'Chaz!'
+            });
+      }
+      google.maps.event.addDomListener(window, 'load', initialize);
+    </script>
+    """
+
+def html_header():
+    return """
+    <head>
+    <title>Where's Chaz?</title>
+    %s
+    </head>
+    """ % google_map(lat, long)
+
+def html_pos_text(lat, long, alt):
+    return """
+    <div class="table">
+    <table>
+    <tr><td>Lat:</td><td>%s</td></tr>
+    <tr><td>Long:</td><td>%s</td></tr>
+    <tr><td>Alt:</td><td>%s</td></tr>
+    </table>
+    </div>
+    """ % (lat, long, alt)
+
+def map_div():
+    return """
+        <div id="map-canvas" />
+        
+    """
+
+def write_page(lat, long, alt):
+    print http_headers()
+    print "<html>"
+    print html_header()
+    print"<body>"
+    print html_pos_text(lat, long, alt)
+    print map_div()
+    print"</body>"
+    print "</html>"
+
 #Read the location
 try:
     (lat, long, alt) = open(LOCATION_FILENAME).readlines()
 except ValueError as e:
     (lat, long, alt) = (0.0, 0.0, 0.0)
-print(lat, long, alt)
+
+lat=lat.strip()
+long=long.strip()
+alt=alt.strip()
+
+write_page(lat, long, alt)
+
